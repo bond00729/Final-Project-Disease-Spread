f25d2af63dfd2be073f3d52f5d9fce54b7b7dc28
Brandon <brandon42496@yahoo.com>
2016-03-09 00:15:30 -0800

    More CSS Changes

--- ui.r
+++ ui.r
@@ -16,6 +16,7 @@ shinyUI(fluidPage(theme = "bootstrap.css",
             font-family: 'Cinzel', serif;
             font-weight: 700;
             line-height: 1.1;
+            text-align: center;
             }
             
           "))
@@ -30,24 +31,33 @@ shinyUI(fluidPage(theme = "bootstrap.css",
         #Added sidebar elements in here
         sidebarPanel(
           sliderInput("Year", "Year:", min = 2005, max = 2013, value = 2005, sep = ""),
+          tags$br(),
+          tags$br(),
           selectInput("disease1", label = h3("Choose a disease"), 
                       choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
                      "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
                      "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
       selected = "Buruli_Ulcer"),
       uiOutput('description1'),
+      tags$br(),
+      tags$br(),
+      tags$br(),
+      tags$br(),
       selectInput("disease2", label = h3("Choose another disease"), 
                   choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
                                  "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
                                  "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
                   selected = "Human_African_Trypanosomiasis"),
-      uiOutput('description2')
+      uiOutput('description2'),
+      tags$br(),
+      tags$br()
       
     ),
     
     #Put main stuff in here
     mainPanel(
       plotlyOutput('map'),
+      tags$br(),
       plotlyOutput('map2')
     )
   )
