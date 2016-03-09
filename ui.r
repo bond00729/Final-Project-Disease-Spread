4c658e16e381df9b8f44d1b8fb24625f43b6335f
Brandon <brandon42496@yahoo.com>
2016-03-09 00:00:23 -0800

    Added Styles to webpage

--- ui.r
+++ ui.r
@@ -5,21 +5,36 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-shinyUI(fluidPage(
-  #Main title panel
-  titlePanel("Spread of Diseases Worldwide"),
-  
-  #Declared a sidebar layout
-  sidebarLayout(
-    
-    #Added sidebar elements in here
-    sidebarPanel(
-      sliderInput("Year", "Year:", min = 2005, max = 2013, value = 2005, sep = ""),
-      selectInput("disease1", label = h3("Choose a disease"), 
-                  choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
-                                 "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
-                                 "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
-                  selected = "Buruli_Ulcer"),
+shinyUI(fluidPage(theme = "bootstrap.css",
+      
+      #Adding CSS to webpage
+      tags$head(
+          tags$style(HTML("
+            @import url('//fonts.googleapis.com/css?family=Cinzel:400,700');
+            
+            h2 {
+            font-family: 'Cinzel', serif;
+            font-weight: 700;
+            line-height: 1.1;
+            }
+            
+          "))
+      ),
+                  
+      #Main title panel
+      titlePanel("Spread of Diseases Worldwide"),
+      
+      #Declared a sidebar layout
+      sidebarLayout(
+        
+        #Added sidebar elements in here
+        sidebarPanel(
+          sliderInput("Year", "Year:", min = 2005, max = 2013, value = 2005, sep = ""),
+          selectInput("disease1", label = h3("Choose a disease"), 
+                      choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
+                     "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
+                     "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
+      selected = "Buruli_Ulcer"),
       uiOutput('description1'),
       selectInput("disease2", label = h3("Choose another disease"), 
                   choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
