8362d069cdb6a0cb2b0f4ff03fb2b59bde7d19fc
powersb <powersb@uw.edu>
2016-03-08 23:18:17 -0800

    Map and text completed

--- server.r
+++ server.r
@@ -6,23 +6,104 @@ library(shiny)
 library(dplyr)
 source("data_wrangling.R")
 
+plotDisease <- function(df, year, dfName){
+  # light grey boundaries
+  l <- list(color = toRGB("black"), width = 0.5)
+  
+  # specify map projection/options
+  g <- list(
+    showframe = FALSE,
+    showcoastlines = TRUE,
+    projection = list(type = 'Mercator')
+  )
+  
+  plot_ly(na.omit(df), z = as.numeric(eval(parse(text = year))), text = as.vector(Country), locations = as.vector(Country), locationmode = 'country names', type = 'choropleth',
+          color = as.numeric(eval(parse(text = year))), colors = 'Reds', marker = list(line = l),
+          colorbar = list(title = 'Number of Cases')) %>%
+    layout(title = gsub('_', ' ', dfName, fixed = TRUE),
+           geo = g)
+}
+
 shinyServer(function(input, output) {
-  output$map <- renderPlotly({ 
-    
-    # light grey boundaries
-    l <- list(color = toRGB("black"), width = 0.5)
-    
-    # specify map projection/options
-    g <- list(
-      showframe = FALSE,
-      showcoastlines = TRUE,
-      projection = list(type = 'Mercator')
-    )
-    
-    plot_ly(na.omit(HAT), z = as.numeric(X2009), text = as.vector(Country), locations = as.vector(Country), locationmode = 'country names', type = 'choropleth',
-            color = as.numeric(X2009), colors = 'Reds', marker = list(line = l),
-            colorbar = list(title = 'Number of Cases')) %>%
-      layout(title = 'Spread of Diseases Worldwide',
-             geo = g)
+  
+  BuruliDesc <- 'Buruli ulcer is a disease caused by infection with the organism Mycobacterium ulcerans mainly affecting 
+  skin but also affecting bone in certain cases. It's found mainly in tropical areas such as West Africa and 
+  Australia. It often results in swelling of affected areas, and large ulcers in later stages. (Mayo Clinic)'
+  HATDesc <- 'Human African Tyrpanosomiasis is a disease transmitted to human through bites from tsetse flies which are found
+  only in rural Africa. Late stages of the disease include behavior changes, confusion, and poor coordination. 
+  (Mayo Clinic)'
+  LeisDesc <- 'Black Fever is a disease caused by infection spread by sandfly bites affecting internal organs such as the spleen, liver, 
+  and bone marrow. It is most common in India. Symptoms may include fever, weight loss, and swelling of spleen or liver. 
+  (Mayo Clinic)'
+  LepDesc <- 'Leprosy is a rare diseases caused by infection with the bacterium Mycobacterium leprae. 80% of cases are found in India, Indonesia, Myanmar, Brazil, and Nigeria. 
+  It mainly affects skin and peripheral and symptoms include pale or red skin lesiobs, reduced sensation, and numbness. (Mayo Clinic)'
+  MalDesc <- 'Malaria is a disease caused by a plasmodium parasite, transmitted through mosquito bites. Symptoms are chills, fever, and sweating.
+  About half of the world's population is at risk with large areas of Africa, South Asia, South America, the Middle East and others 
+  among the most common locations of outbreaks. (Mayo Clinic & Center for Disease Control and Prevention)'
+  OnchDesc <- 'Onchocerciasis is a parasitic, tropical disease spread by repeated bites from infected black flies. It's most common in remote African villages. 
+  Symptoms usually don't appear for one to two years after infection, but include skin nodules, itching, eye infections, and blindness. (Mayo Clinic)'
+  
+  createText1 <- eventReactive(input$disease1,  {
+    if (input$disease1 == "Buruli_Ulcer") {
+      return(BuruliDesc)
+    }
+    if (input$disease1 == "Human_African_Trypanosomiasis") {
+      return(HATDesc)
+    }
+    if (input$disease1 == "Leishmaniasis") {
+      return(LeisDesc)
+    }
+    if (input$disease1 == "Leprosy") {
+      return(LepDesc)
+    }
+    if (input$disease1 == "Malaria") {
+      return(MalDesc)
+    }
+    if (input$disease1 == "Onchocerciasis") {
+      return(OnchDesc)
+    }
+  })
+  
+  createText2 <- eventReactive(input$disease2,  {
+    if (input$disease2 == "Buruli_Ulcer") {
+      return(BuruliDesc)
+    }
+    if (input$disease2 == "Human_African_Trypanosomiasis") {
+      return(HATDesc)
+    }
+    if (input$disease2 == "Leishmaniasis") {
+      return(LeisDesc)
+    }
+    if (input$disease2 == "Leprosy") {
+      return(LepDesc)
+    }
+    if (input$disease2 == "Malaria") {
+      return(MalDesc)
+    }
+    if (input$disease2 == "Onchocerciasis") {
+      return(OnchDesc)
+    }
+  })
+  
+  output$map <- renderPlotly({
+    A <- paste0('X', input$Year)
+    Name1 <- input$disease1
+    Dis1 <- eval(parse(text = Name1))
+    plotDisease(Dis1, A, Name1)
+  })
+  
+  output$map2 <- renderPlotly({
+    B <- paste0('X', input$Year)
+    Name2 <- input$disease2
+    Dis2 <- eval(parse(text = Name2))
+    plotDisease(Dis2, B, Name2)
+  })
+  
+  output$description1 <- renderUI({
+    helpText(createText1())
+  })
+  
+  output$description2 <- renderUI({
+    helpText(createText2())
   })
 })
--- ui.r
+++ ui.r
@@ -14,19 +14,26 @@ shinyUI(fluidPage(
     
     # Put sidebar elements in here
     sidebarPanel(
-      sliderInput("Year", "Year:", min = 1991, max = 2014, value = 2000, sep = ""),
-      checkboxGroupInput("Disease", label = h3("Disease"), 
-                         choices = c("Buruli Ulcer" = "Buruli Ulcer",
-                                     "Black Fever (Leishmaniasis)" = "Leishmaniasis",
-                                     "HAT" = "HAT",
-                                     "Leprosy" = "Leprosy",
-                                     "Malaria" = "Malaria",
-                                     "River Blindness" = "River Blindness"))
+      sliderInput("Year", "Year:", min = 2005, max = 2013, value = 2005, sep = ""),
+      selectInput("disease1", label = h3("Choose a disease"), 
+                  choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
+                                 "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
+                                 "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
+                  selected = "Buruli_Ulcer"),
+      uiOutput('description1'),
+      selectInput("disease2", label = h3("Choose another disease"), 
+                  choices = list("Buruli Ulcer" = "Buruli_Ulcer", "Human African Trypanosomiasis" = "Human_African_Trypanosomiasis", 
+                                 "Leishmaniasis" = "Leishmaniasis", "Leprosy" = "Leprosy",
+                                 "Malaria" = "Malaria", "Onchocerciasis" = "Onchocerciasis"), 
+                  selected = "Human_African_Trypanosomiasis"),
+      uiOutput('description2')
+      
     ),
     
     #Put main stuff in here
     mainPanel(
-      plotlyOutput('map')
+      plotlyOutput('map'),
+      plotlyOutput('map2')
     )
   )
 ))
